db8fae699f633cf74635d90ec7c7bd70bbaee4a3
qiyeb <quyebl@foxmail.com>
2025-12-24 17:07:29 +0800

    Update forward.tsx

--- vite-frontend/src/pages/forward.tsx
+++ vite-frontend/src/pages/forward.tsx
@@ -1304,12 +1304,13 @@ export default function ForwardPage() {
             <Button
               size="sm"
               variant="flat"
-              color="warning"
-              onPress={() => handleDiagnose(forward)}
+              color="danger"
+              onPress={() => handleDelete(forward)}
               className="flex-1 min-h-8"
               startContent={
                 <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                  <path fillRule="evenodd" d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" clipRule="evenodd" />
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8 7a1 1 0 012 0v4a1 1 0 11-2 0V7zM12 7a1 1 0 012 0v4a1 1 0 11-2 0V7z" clipRule="evenodd" />
                 </svg>
               }
             >
